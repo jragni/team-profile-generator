be47a8453f89bd26e8a0f21489cf0a86a2874f1d
Jhensen Agni <jagni001@ucr.edu>
2020-01-26 22:30:09 -0800

    classes complete. adding Assets

---
+++ app.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
+
+
